87bb9b652f59fd52ccd426d7d13499e580d2a2d9
JeffLu <jeff80119@gmail.com>
2020-05-04 11:45:04 +0800

    add write to csv

--- product.py
+++ product.py
@@ -4,9 +4,17 @@ while True:
     if name == 'q':
     	break
     price = input('請輸入商品價格：')
+    price = int(price)
     # p = []
     # p.append(name)
     # p.append(price)
     # p = [name, price]
     products.append([name, price])
 print(products)
+
+for p in products:
+	print(p)
+
+with open ('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
